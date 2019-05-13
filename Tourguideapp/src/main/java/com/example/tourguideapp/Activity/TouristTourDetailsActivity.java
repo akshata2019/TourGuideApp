adc460452d5f1184c66d5da2453e56c597ae47cf
Shweta Rao <Shwetar@hexaware.com>
2019-05-13 17:36:39 +0530

    Tour Guid app for tourist and guide

--- Tourguideapp/src/main/java/com/example/tourguideapp/Activity/TouristTourDetailsActivity.java
+++ Tourguideapp/src/main/java/com/example/tourguideapp/Activity/TouristTourDetailsActivity.java
@@ -4,12 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.tourguideapp.R;
 
 public class TouristTourDetailsActivity extends Activity {
     TextView guidetext;
+    ImageView backImage;
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +21,15 @@ public class TouristTourDetailsActivity extends Activity {
             @Override
             public void onClick(View v) {
                 Intent i =new Intent(TouristTourDetailsActivity.this,TourGuideActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        backImage=findViewById(R.id.backImage);
+        backImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent(TouristTourDetailsActivity.this,TouristTourActivity.class);
                 startActivity(i);
                 finish();
             }
